cfa8d59a554ea0d9eefe999a6e87124af2562bc9
Elson Jose <elsonjoseofficial@gmail.com>
2019-11-18 21:06:40 +0530

    Modified build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,5 @@
-#nasm -f elf32 kernel.asm -o kasm.o  
+nasm -f elf32 kernel.asm -o kasm.o  
 gcc -m32 -c kernel.c -o kc.o        
-ld -m elf_i386 -T link.ld -o kernel.bin kasm.o kc.o 
-qemu-system-i386 -net user -kernel kernel.bin 
+ld -m elf_i386 -T link.ld -o Slate/boot/kernel.bin kasm.o kc.o 
+qemu-system-i386 -net user -kernel Slate/boot/kernel.bin 
+grub-mkrescue -o Slate.iso Slate/
