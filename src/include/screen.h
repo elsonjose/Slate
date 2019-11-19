3b7ff7dd1a6cb9ca73b626a4ab9ba34036634f40
Elson Jose <elsonjoseofficial@gmail.com>
2019-11-19 19:57:43 +0530

    created ultimate terminal

--- src/include/screen.h
+++ src/include/screen.h
@@ -3,109 +3,15 @@
 #include "types.h"
 #include "system.h"
 #include "string.h"
-int cursorX =0 , cursorY=0;
-const uint8 sw=80,sh=25,sd=2;
-void clearLine(uint8 from,uint8 to)
-{
-    uint16 i = sw * from * sd;
-    string vidmem = (string) 0xb8000;
-    for(i;i<(sw*(to+1)*sd);i++)
-    {
-        vidmem[i]=0x0;
-    }
-} 
-void updateCursor()
-{
-    unsigned temp;
-    temp= cursorY * sw + cursorX;
-    outportb(0x3D4, 14);
-    outportb(0x3D5, temp >> 8);
-    outportb(0x3D4, 15);
-    outportb(0x3D5, temp);
-}
 
-void clearScreen()
-{
-    clearLine(0,sh-1);
-    cursorX = 0;
-    cursorY = 0;
-    updateCursor();
-}
+int cursorX , cursorY;
+const uint8 sw,sh,sd;
+void clearLine(uint8 from,uint8 to);
+void updateCursor();
+void clearScreen();
+void  scrollUp(uint8 lineNumber);
+void newLineCheck();
+void printch(char c);
+void print(string ch);
 
-void  scrollUp(uint8 lineNumber)
-{
-    string vidmem = (string) 0xb8000;
-    uint16 i=0;
-    for(i;i<sw*(sh-1)*2;i++)
-    {
-        vidmem[i] = vidmem[i+sw*2*lineNumber];
-    }
-    clearLine(sh-1-lineNumber,sh-1);
-    if((cursorY-lineNumber) <0)
-    {
-        cursorX=0;
-        cursorY=0;
-    }
-    else
-    {
-        cursorY -=lineNumber;
-    }
-    updateCursor();
-}
-
-void newLineCheck()
-{
-    if(cursorY >= sh-1)
-    {
-        scrollUp(1);
-    }
-}
-
-void printch(char c)
-{
-    string vidmem = (string) 0xb8000;
-    switch (c)
-    {
-        case(0x08):
-            if(cursorX > 0)
-            {
-                cursorX--;
-                vidmem[(cursorY*sw+cursorX)*sd]=0x00;
-            } 
-            break;
-        case(0x09):
-            cursorX= (cursorX+8) & ~(8-1);
-            break;
-        case('\r'):
-            cursorX=0;
-            break;
-        case('\n'):
-            cursorX=0;
-            cursorY++;
-            break;
-        default:
-            vidmem[((cursorY*sw+cursorX))*sd]=c;
-            vidmem[((cursorY*sw+cursorX))*sd+1]=0x0F;
-            cursorX++;
-            break;
-    }
-
-    if (cursorX >= sw)
-    {
-        cursorX=0;
-        cursorY++;
-    }
-    newLineCheck();
-    updateCursor();
-    
-}
-
-void print(string ch)
-{
-    uint16 i=0;
-    for(i;i<strlength(ch);i++)
-    {
-        printch(ch[i]);
-    }
-}
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,34 @@
 #include "include/screen.h"
+#include "include/kb.h"
+#include "include/string.h"
 kmain()
 {
+    clearScreen();
+    print("Slate OS Active\nType help for all commands\n\n");
+    while (1)
+    {
+        print("\n@Slate> ");
+        string ch = readStr();
+        if(strEql(ch,"ucmd"))
+        {
+            print("\nalready in an ultimate terminal");
+
+        }
+        else if(strEql(ch,"help"))
+        {
+            print("\nucmd :   for starting a new ultimate terminal");
+            print("\nclr :   for clearing the screen");
+        }
+        else if(strEql(ch,"clr"))
+        {
+            clearScreen();    
+        }
+        else
+        {
+           print("\ncommand not found\n");
+        }
+        
+        print("\n");
+    }
     
-    print("HELLO");
 }
